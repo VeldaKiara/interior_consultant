baf38c6c6ce8ec8a897543a9817e4826414795a5
veldakarimi <veldakarimi@gmail.com>
2021-09-09 23:30:21 +0300

    updated nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,22 @@
 import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import './Navbar.css';
+
 const Nav = () => {
   return (
     <div>
-      <div className="flex">
-        <div className="text-white font-serif text-sm border-4 h-12 w-40 mx-28  text-center pt-2 mt-10">
-          {" "}
-          THIS INTERIOR{" "}
+      <div className="flex nav">
+        <div className="text-white font-serif text-sm border-4 h-12 w-40  text-center pt-2 flex-none">
+          THIS INTERIOR
         </div>
-        <div>
+        <div className = "text-white nav-items font-serif text-sm flex flex-row flex-grow "> 
           <Router>
-            <NavLink to="/"> Home </NavLink>
-            <NavLink to="/"> Collection </NavLink>
-            <NavLink to="/"> About </NavLink>
-            <NavLink to="/"> Contact </NavLink>
+           <div class="nav-item"> <NavLink to="/"> Home </NavLink> </div>
+
+           <div class="nav-item"><NavLink to="/"> Collection </NavLink> </div>
+
+           <div class="nav-item"><NavLink to="/"> About </NavLink></div>
+
+           <div class="nav-item"> <NavLink to="/"> Contact </NavLink></div>
             </Router>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    flexGrow: {
+      DEFAULT: 2,
+      '1': 1,
+    },
     extend: {},
   },
    variants: {
