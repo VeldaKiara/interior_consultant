02ae673753e7c293318449062a1b12587ec06c0a
veldakarimi <veldakarimi@gmail.com>
2021-10-09 10:19:05 +0300

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,18 @@ const Nav = () => {
   return (
     <div>
       <div className="flex nav">
-        <div className="text-white font-serif text-sm border-4 h-12 w-40  text-center pt-2 flex-none">
+        <div className="text-white font-serif text-sm border-4 h-12 w-40 text-center pt-2 flex-none">
           THIS INTERIOR
         </div>
         <div className = "text-white nav-items font-serif text-sm flex flex-row flex-grow "> 
           <Router>
-           <div class="nav-item"> <NavLink to="/"> Home </NavLink> </div>
+           <div className="nav-item  border-b-0 nav-item-h"> <NavLink to="/"> Home </NavLink> </div>
 
-           <div class="nav-item"><NavLink to="/"> Collection </NavLink> </div>
+           <div className="nav-item nav-item-c"><NavLink to="/"> Collection </NavLink> </div>
 
-           <div class="nav-item"><NavLink to="/"> About </NavLink></div>
+           <div className="nav-item nav-item-a"><NavLink to="/"> About </NavLink></div>
 
-           <div class="nav-item"> <NavLink to="/"> Contact </NavLink></div>
+           <div className="nav-item nav-item-co"> <NavLink to="/"> Contact </NavLink></div>
             </Router>
         </div>
       </div>
