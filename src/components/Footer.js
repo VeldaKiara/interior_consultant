c458b2b7caafdc4cb186221812068879bce22cc2
veldakarimi <veldakarimi@gmail.com>
2022-01-19 00:03:28 +0300

    footer ui update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import "../assets/styles/Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-        {/* <p className= "footer-ft sticky bottom-0" > created by Velda Kiara - devChallenges.io </p>  */}
-       <p className="footer-ft sticky bottom-0"> created by <a href='https://twitter.com/VeldaKiara' > <b> <u> Velda Kiara </u></b> </a> - devChallenges.io </p>
+       <p className="footer-ft sticky bottom-0"> created by <a href='https://twitter.com/VeldaKiara'> <b> <u> Velda Kiara </u></b> </a> - devChallenges.io </p>
         </div>
     );
 }
